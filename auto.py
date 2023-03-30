3c0fa813218fe630a4058010cda6b18318615e3b
Tom Barratt <tom_barratt@outlook.com>
2023-03-30 12:07:51 +0100

    Added status, decreasing waits, emergency & scan

--- auto.py
+++ auto.py
@@ -6,6 +6,7 @@ import math
 from RobotClass import Robot
 import time
 from tf2_msgs.msg import TFMessage
+from typing import Tuple
 
 robot = Robot()
 emergency_stop = False
@@ -16,7 +17,12 @@ prev_angular = 0
 distance = 0
 prev_calc = time.time()
 
-def euler_from_quaternion(x, y, z, w):
+
+rospy.init_node('listener', anonymous=True, log_level=rospy.INFO)
+
+status = "Ready"
+
+def euler_from_quaternion(x : float, y : float, z : float, w : float) -> Tuple[float, float, float]:
         t0 = +2.0 * (w * x + y * z)
         t1 = +1.0 - 2.0 * (x * x + y * y)
         roll_x = math.atan2(t0, t1)
@@ -66,12 +72,13 @@ def euler_from_quaternion(x, y, z, w):
 
 
 
-def pointcb(point_msg):
-    global emergency_stop
+
+def pointcb(point_msg : PointStamped) -> None:
+    global emergency_stop, status
     print("Received goal point")
 
-    def turn_to_goal(goal_x, goal_y):
-        global emergency_stop
+    def turn_to_goal(goal_x : float, goal_y : float) -> None:
+        global emergency_stop, status
         print("Turning to goal point")
         odo_msg = rospy.wait_for_message('/odometry/filtered', Odometry)
         euler_x, euler_y, euler_z = euler_from_quaternion(odo_msg.pose.pose.orientation.x, odo_msg.pose.pose.orientation.y, odo_msg.pose.pose.orientation.z, odo_msg.pose.pose.orientation.w)
@@ -86,24 +93,30 @@ def pointcb(point_msg):
 
         goal_angle = math.degrees(math.atan2(change_in_y, change_in_x))
         print(f"Goal angle: {goal_angle}, changed needed: {goal_angle - euler_z}")
-
-        while (round(goal_angle - euler_z, 0) != 0) and (not emergency_stop):
+        status = f"Rotation Remaining : {goal_angle - euler_z} degrees"
+        while (abs(goal_angle - euler_z) >= 1) and (not emergency_stop):
             odo_msg = rospy.wait_for_message('/odometry/filtered', Odometry)
             euler_x, euler_y, euler_z = euler_from_quaternion(odo_msg.pose.pose.orientation.x, odo_msg.pose.pose.orientation.y, odo_msg.pose.pose.orientation.z, odo_msg.pose.pose.orientation.w)
             print(f"Current angle: {euler_z}, goal angle: {goal_angle}, delta: {goal_angle - euler_z}")
+            status = f"Rotation Remaining : {goal_angle - euler_z} degrees"
             if goal_angle - euler_z < 0:
                 # print(f"Right : {goal_angle - euler_z}")
                 robot.right(0.35)
             else:
                 # print(f"Left : {goal_angle - euler_z}")
                 robot.left(0.35)
-            rospy.sleep(0.1)
+
+            amount = abs(goal_angle - euler_z) / 20
+            rospy.sleep(min(amount, 5))
             robot.stop()
-        print(f"done, final deviation: {goal_angle - euler_z}")
+        if emergency_stop:
+            print("Stopping due to emergency")
+        else:
+            print(f"done, final deviation: {goal_angle - euler_z}")
 
 
-    def move_to_goal(goal_x, goal_y):
-        global emergency_stop
+    def move_to_goal(goal_x : float, goal_y : float) -> bool:
+        global emergency_stop, status
         print("Moving to goal point")
         odo_msg = rospy.wait_for_message('/odometry/filtered', Odometry)
         change_in_x = goal_x - odo_msg.pose.pose.position.x
@@ -113,18 +126,23 @@ def pointcb(point_msg):
         prev_distance = 1000
         distance = math.sqrt(change_in_x**2 + change_in_y**2)
         while (distance > 0.1) and (not emergency_stop):
+            status = f"Distance Remaining : {distance} meters"
             odo_msg = rospy.wait_for_message('/odometry/filtered', Odometry)
             change_in_x = goal_x - odo_msg.pose.pose.position.x
             change_in_y = goal_y - odo_msg.pose.pose.position.y
             distance = math.sqrt(change_in_x**2 + change_in_y**2)
             deviation = distance - prev_distance
-            if deviation > 0.01:
+            if deviation > 0.10:
                 print(f"Overshoot by distance: {distance - prev_distance}")
+                robot.stop()
                 return False
             prev_distance = distance
             print(f"Current distance: {distance}")
-            robot.forward(0.3)
-            rospy.sleep(0.1)
+            robot.forward(0.25)
+
+            amount = distance * 3
+            rospy.sleep(min(amount, 5))
+            robot.stop()
         print(f"done, final deviation: {distance}")
         return True
 
@@ -132,58 +150,92 @@ def pointcb(point_msg):
     turn_to_goal(point_msg.point.x, point_msg.point.y)
     while (not move_to_goal(goal_x, goal_y)) and (not emergency_stop):
         turn_to_goal(point_msg.point.x, point_msg.point.y)
+        status = "Turning Complete, Moving to Goal Point"
         rospy.sleep(0.1)
+    status = "Moving Complete"
     robot.stop()
     emergency_stop = False
 
 
-def loco_cb(data):
+def loco_cb(data : Twist) -> None:
+    global emergency_stop, collision_stop, status
     print(f"Linear: {data.linear.x}, Angular: {data.angular.z}")
     global prev_linear, prev_angular
+    if emergency_stop:
+        print("Emergency stop active, not moving")
+        return
     if data.linear.x != prev_linear:
+        if data.linear.x > 0:
+            status = f"Moving forward at speed {data.linear.x}"
+        elif data.linear.x < 0:
+            status = f"Moving backward at speed {-data.linear.x}"
+            
         prev_linear = data.linear.x
         robot.forward(data.linear.x)
     if data.angular.z != prev_angular:
+        if data.angular.z < 0:
+            status = f"Turning right at speed {-data.angular.z}"
+        elif data.angular.z > 0:
+            status = f"Turning left at speed {data.angular.z}"
         prev_angular = data.angular.z
         robot.left(data.angular.z)
 
-def emergency_cb():
-    global emergency_stop
-    emergency_stop = True
-    robot.stop()
+def emergency_cb(data : Empty) -> None:
+    global emergency_stop, status
 
-def collision_cb():
-    global collision_stop
-    global emergency_stop
+    status = f"E-Stop Received"
+    print("Emergency stop received, stopping robot")
+    emergency_stop = True
+    #robot.stop()
+
+def collision_cb(data : Empty) -> None:
+    global collision_stop, emergency_stop, status
+    status = f"Possible Collision Detected"
+    print("Collision detected, stopping robot")
     collision_stop = True
     emergency_stop = True
-    robot.stop()
+    #robot.stop()
+
+def scan_cb(data : Empty) -> None:
+    global emergency_stop, status
+    rotation_count = 25
+
+    for i in range(rotation_count):
+        status = f"Scanning {i+1}/{rotation_count}"
+        robot.left(0.4)
+        rospy.sleep(0.2)
+        if emergency_stop:
+            break
+        robot.stop()
+        rospy.sleep(0.4)
+        if emergency_stop:
+            break
 
 
-def listener():
-    rospy.init_node('listener', anonymous=True, log_level=rospy.INFO)
-    rospy.loginfo("Listener node initialized, subscribing to emergency & collision...")
+def listener() -> None:
+    global emergency_stop, collision_stop, status
     rospy.Subscriber("emergency", Empty, emergency_cb)
-    rospy.Subscriber("collision_stop", Empty, emergency_cb)
-    rospy.loginfo("Subscribed, subscribing to loco...")
+    rospy.Subscriber("collision_stop", Empty, collision_cb)
     rospy.Subscriber("loco", Twist, loco_cb)
-    rospy.loginfo("Subscribed, subscribing to goto...")
     rospy.Subscriber('goto', PointStamped, pointcb)
+    rospy.Subscriber('scan', Empty, scan_cb)
+    print("Subscribed to topics")
 
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
         if emergency_stop:
             rospy.loginfo("Emergency stop, stopping robot")
             robot.stop()
-            emergency_stop = False
 
         if collision_stop:
             rospy.loginfo("Collision stop, reversing robot")
-            robot.backward()
-            rospy.sleep(0.5)
+            robot.backward(0.3)
+            time.sleep(0.25)
             robot.stop()
             collision_stop = False
-
+        
+        status_pub = rospy.Publisher('/robot/status', String, queue_size=50)
+        status_pub.publish(status)
         rate.sleep()
 
 if __name__ == '__main__':
--- emergency.py
+++ emergency.py
@@ -1,4 +1,8 @@
 import rospy
+from sensor_msgs.msg import Image
+from std_msgs.msg import String, Empty
+import numpy as np
+
 
 def depth_cb(data):
     global distance, prev_calc
@@ -15,3 +19,15 @@ def depth_cb(data):
     np_arr[np_arr < 100] = 0
     np_arr[0,0] = 2000
     distance = np_arr[np_arr!=0].min()
+
+    if distance < 500:
+        print("Emergency stop - distance: ", distance)
+        pub = rospy.Publisher('/collision_stop', Empty, queue_size=1)
+        pub.publish(Empty())
+    else:
+        print("Distance: ", distance)
+        
+rospy.init_node('collision_checker', anonymous=True)
+rospy.Subscriber("camera/depth/image_rect_raw", Image, depth_cb)
+rospy.spin()
+
